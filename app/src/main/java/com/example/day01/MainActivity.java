d0bc7a30864bd16b78e4bdde99c6403d78f6c96b
shengli020407 <151366814@qq.com>
2020-12-15 19:35:23 +0800

    王胜利修改提交

--- app/src/main/java/com/example/day01/MainActivity.java
+++ app/src/main/java/com/example/day01/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+	
+	//祝老板旗开得胜
+
     }
 }
